daa0faac10bf1ed0640cc9785633a79e5c0331ee
Christopher Hall <1167959+ArkahnX@users.noreply.github.com>
2025-12-08 12:23:29 -0800

    update package.json and harden honeypot code against long messages

--- src/core/events/messageCreate.ts
+++ src/core/events/messageCreate.ts
@@ -1,6 +1,8 @@
 import * as dotenv from 'dotenv'
 dotenv.config({ path: __dirname + '/../.env' })
 import {
+  AttachmentBuilder,
+  codeBlock,
   EmbedBuilder,
   GuildMember,
   inlineCode,
@@ -38,34 +40,8 @@ export async function messageCreate(msg: Message): Promise<void> {
   if (permLevel.level >= 2) return
 
   log(oneLine`${author.user.username} (${author.id}) triggered the honeypot`)
-  const media = msg.attachments.size > 0 ? msg.attachments.map((a) => a.url).join('\n') : ''
-  const embed = new EmbedBuilder()
-    .setAuthor({ name: author.user.username, iconURL: author.displayAvatarURL() })
-    .setDescription(
-      'User triggered the honeypot... \nUse the `/honeypot` command to enable/disable the honeypot',
-    )
-    .addFields(
-      { name: 'User', value: userMention(author.id), inline: true },
-      { name: 'ID', value: inlineCode(author.id), inline: true },
-      {
-        name: 'Media',
-        value: media.length ? msg.attachments.size.toString() : 'None',
-        inline: true,
-      },
-    )
-    .setTimestamp(new Date())
-  if (media !== '') {
-    embed.addFields({ name: 'Attachments', value: media })
-  }
-  if (msg.content !== '') {
-    embed.addFields({ name: 'Message', value: msg.content })
-  }
-  if (bot) {
-    embed.addFields({ name: 'Command', value: `Used a command from ${bot.username}` })
-  }
-  if (msg.poll) {
-    embed.addFields({ name: 'Message', value: 'Made a poll' })
-  }
+  let successfulBan = false
+  let banningDisabled = false
   const settings = getSettings(msg.guild!)
   const honeypot = settings.honeypot
 
@@ -77,9 +53,9 @@ export async function messageCreate(msg: Message): Promise<void> {
       // const results = { bannedUsers: [] }
       if (results?.bannedUsers.length === 0) {
         log(oneLine`could not ban user ${author.id}`)
-        embed.setTitle('Failed to ban user')
       } else {
-        embed.setTitle('Successfully banned user')
+        log(oneLine`successfully banned user ${author.id}`)
+        successfulBan = true
         // await msg.guild?.bans.remove(author.id)
       }
     } catch (err) {
@@ -87,16 +63,63 @@ export async function messageCreate(msg: Message): Promise<void> {
     }
   } else {
     log(oneLine`banning users is disabled ${author.id}`)
-    embed.setTitle('Banning users is disabled')
-    embed.setDescription(
-      'Honeypot triggered. Use the `/honeypot` command to enable banning non-mod users',
-    )
+    banningDisabled = true
   }
 
   const logChannel = await msg.guild?.channels.fetch(process.env.DISCORD_HONEYPOT_LOG_CHANNEL_ID)
   if (logChannel?.isSendable() && logChannel?.isTextBased()) {
     try {
-      await logChannel?.send({ embeds: [embed] })
+      const media = msg.attachments.size > 0 ? msg.attachments.map((a) => a.url).join('\n') : ''
+      const embed = new EmbedBuilder()
+        .setAuthor({ name: author.user.username, iconURL: author.displayAvatarURL() })
+        .setDescription(
+          'User triggered the honeypot... \nUse the `/honeypot` command to enable/disable the honeypot',
+        )
+        .addFields(
+          { name: 'User', value: userMention(author.id), inline: true },
+          { name: 'ID', value: inlineCode(author.id), inline: true },
+          {
+            name: 'Media',
+            value: media.length ? msg.attachments.size.toString() : 'None',
+            inline: true,
+          },
+        )
+        .setTimestamp(new Date())
+      if (successfulBan) {
+        embed.setTitle('Successfully banned user')
+      } else {
+        embed.setTitle('Failed to ban user')
+      }
+      if (banningDisabled) {
+        embed.setTitle('Banning users is disabled')
+        embed.setDescription(
+          'Honeypot triggered. Use the `/honeypot` command to enable banning non-mod users',
+        )
+      }
+      if (media !== '') {
+        embed.addFields({ name: 'Attachments', value: media })
+      }
+      if (msg.content !== '') {
+        if (msg.content.length > 1000) {
+          embed.addFields({ name: 'Message', value: 'Message too long, see attachment' })
+        } else {
+          embed.addFields({ name: 'Message', value: codeBlock(msg.content) })
+        }
+      }
+      if (bot) {
+        embed.addFields({ name: 'Command', value: `Used a command from ${bot.username}` })
+      }
+      if (msg.poll) {
+        embed.addFields({ name: 'Message', value: 'Made a poll' })
+      }
+      if (msg.content && msg.content.length > 1000) {
+        let attachment = new AttachmentBuilder(Buffer.from(msg.content, 'utf-8')).setName(
+          'message.txt',
+        )
+        await logChannel?.send({ embeds: [embed], files: [attachment] })
+      } else {
+        await logChannel?.send({ embeds: [embed] })
+      }
     } catch (err) {
       debug('Unable to send message to log channel: ' + err)
     }
